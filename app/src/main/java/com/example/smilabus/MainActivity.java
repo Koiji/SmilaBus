0d908db408e1e412034dc747506584a62c111efb
Koiji <55895174+Koiji@users.noreply.github.com>
2021-02-19 09:06:29 +0200

    New Init

--- app/src/main/java/com/example/smilabus/MainActivity.java
+++ app/src/main/java/com/example/smilabus/MainActivity.java
@@ -3,21 +3,26 @@ package com.example.smilabus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //Не трогай массив, а то сомаешь!!
+    //Не трогай массив, а то сlомаешь!!
     String[] nums = {"3", "4", "5", "9", "10",
-                    "13", "17", "18", "30",
-                    "32", "34", "35", "36",
-                    "37", "39", "40", "41",
-                    "45", "48", "49"};
+            "13", "17", "18", "30",
+            "32", "34", "35", "36",
+            "37", "39", "40", "41",
+            "45", "48", "49"};
+
+    Button Poputki;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -28,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Применяем адаптер к элементу spinner
         spinner.setAdapter(adapter);
+
+        Poputki = (Button) findViewById(R.id.Poputki);
+        Poputki.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Poputki:
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/smilabus/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.smilabus;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
